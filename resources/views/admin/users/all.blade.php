f825b93a6ff977c9d19e5955e5b6441232e0063a
mohosin16103308 <hamza1610330816@gmail.com>
2021-01-19 22:47:37 +0600

    Admin Login Access Modifier Check

---
+++ resources/views/admin/users/all.blade.php
@@ -0,0 +1,141 @@
+ 
+@extends('dashboard.index')
+
+@section('title') Admin @stop
+
+
+@section('main_body')
+ 
+ <!-- Main Content -->
+ <div id="content">
+
+    <!-- Topbar -->
+    <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+        <!-- Sidebar Toggle (Topbar) -->
+        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+            <i class="fa fa-bars"></i>
+        </button>
+
+        <!-- Topbar Search -->
+       
+        <!-- Topbar Navbar -->
+        <ul class="navbar-nav ml-auto">
+
+            <!-- Nav Item - Search Dropdown (Visible Only XS) -->
+          
+
+          
+
+       
+
+            <div class="topbar-divider d-none d-sm-block"></div>
+
+            <!-- Nav Item - User Information -->
+            <li class="nav-item dropdown no-arrow">
+                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                    <img class="img-profile rounded-circle"
+                        src="img/undraw_profile.svg">
+                </a>
+                <!-- Dropdown - User Information -->
+                <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                    aria-labelledby="userDropdown">
+                    <a class="dropdown-item" href="#">
+                        <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                        Profile
+                    </a>
+                    <a class="dropdown-item" href="#">
+                        <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                        Settings
+                    </a>
+                  
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                        Logout
+                    </a>
+                </div>
+            </li>
+
+        </ul>
+
+    </nav>
+    <!-- End of Topbar -->
+
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+
+
+         {{-- Display Error Message  --}}
+         <div class="row">
+            <div class="col-md-12">
+        
+               {{-- Display Error Message  --}}
+              @include('admin.error.error')
+            
+            </div>
+          </div>
+          
+
+        <div class="row">
+            <div class="col-md-12">
+
+             <table id="example" class="display">
+              <thead>
+                  <tr>
+                      <th>ID</th>
+                      <th>Name</th>
+                      <th>Area Code</th>
+                      <th>Phone</th>
+                      <th>Email </th>
+                      <th> Action </th>
+                  </tr>
+              </thead>
+              <tbody>
+                 @foreach ($sellers as $seller)
+                     
+                
+                  <tr>
+                      <td>{{$seller->id}}</td>
+                      <td>{{$seller->name}}</td>
+                      <td>{{$seller->area_code}}</td>
+                      <td>{{$seller->phone}}</td>
+                      <td>{{$seller->email}}</td>
+                      <td>
+                          <div class="action">
+                            <a href="{{route('admin.seller_editadmin_seller_edit',$seller->id)}}"><i class="fas fa-edit" data-toggle="tooltip" data-placement="top" title="Seller Edit"></i></a>
+                            <a href="{{route('admin.seller_viewadmin_seller_show',$seller->id)}}" class="text-success"><i class="fas fa-eye" data-toggle="tooltip" data-placement="top" title="Seller View"></i></a>
+                            <a href="{{ route('admin.seller_destroyadmin_seller_destroy',$seller->id) }}" onclick="return confirm('Are You Sure to Delete?')" class="text-warning"><i class="fas fa-trash-alt" data-toggle="tooltip" data-placement="top" title="Seller Delete"></i></a>
+                        </div>
+                      </td>
+                  </tr>
+             
+                  @endforeach
+             
+              </tbody>
+              <tfoot>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Area Code</th>
+                    <th>Phone</th>
+                    <th>Email </th>
+                    <th> Action </th>
+                </tr>
+              </tfoot>
+          </table>
+            </div>
+
+    </div>
+    <!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+
+
+ @stop
+ 
---
+++ resources/views/dashboard/include/sidebar.blade.php
@@ -0,0 +1,69 @@
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('admin.dashboardadmin_dashboard') }}">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3"> {{  Session::get('admin_name') }}</div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <a class="nav-link" href="{{ route('admin.dashboardadmin_dashboard') }}">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>{{  Session::get('admin_phone') }}</span></a>
+            </li>
+
+          
+
+           {{--  Access For Admin   --}}
+
+        @if (Session::get('admin_is_login'))
+              
+
+           <!-- Nav Item - Seller Settings  Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseSellers"
+                    aria-expanded="true" aria-controls="collapseSellers">
+                    <i class="fas fa-fw fa-folder"></i>
+                    <span>Seller Settings</span>
+                </a>
+                <div id="collapseSellers" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item" href="{{ route('admin.seller_createadmin_seller_create') }}">Add New</a>
+                        <a class="collapse-item" href="{{ route('admin.seller_alladmin_seller_all') }}">All Seller</a>
+                    </div>
+                </div>
+            </li>
+
+        @endif
+
+            <!-- Nav Item - Admin Settings  Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseCustomers"
+                    aria-expanded="true" aria-controls="collapseCustomers">
+                    <i class="fas fa-fw fa-folder"></i>
+                    <span>Customer Settings</span>
+                </a>
+                <div id="collapseCustomers" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item" href="{{ route('seller.customer_createseller_seller_create') }}">Add New</a>
+                        <a class="collapse-item" href="{{ route('seller.customer_allseller_seller_all') }}">All Customer</a>
+                    </div>
+                </div>
+            </li>
+
+            <!-- Nav Item - Charts -->
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('admin.admin_settingsadmin_settings') }}">
+                    <i class="fas fa-fw fa-chart-area"></i>
+                    <span>Settings</span></a>
+            </li>
+
+        </ul>
+        <!-- End of Sidebar -->
